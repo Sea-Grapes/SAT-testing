7adfe028ecb3493b6e8b54bf784833a9dd7d3277
Sea Grapes <natuloid@gmail.com>
2024-06-25 14:30:23 -0700

    .

--- fetch.js
+++ fetch.js
@@ -1,8 +1,30 @@
-fetch(`https://sat-admin-dates.collegeboard.org/`).then(res => res.json()).then(days => {
-  days = days.map(e => e.eventFormattedDate)
-  window.days = days
-  console.log(days)
 
-  days.forEach(day => fetch(`https://aru-test-center-search.collegeboard.org/prod/test-centers?date=${day}&zip=98075&country=US`))
-})
-.catch(e => alert('Error with CollegeBoard API'))
+
+try {
+  fetch(`https://sat-admin-dates.collegeboard.org/`).then(res => res.json()).then(sessions => {
+    data = {}
+
+    sessions.forEach(({
+      eventFormattedDate: dateAPI,
+      eventDisplayDate: displayDate,
+    }) => {
+      fetch(`https://aru-test-center-search.collegeboard.org/prod/test-centers?date=${dateAPI}&zip=${zipInput.value}&country=US`)
+      .then(res => res.json())
+      .then(schools => {
+        schools = schools
+        .filter(school => school.distance <= distanceInput.value)
+        .filter(school => school.seatAvailability)
+        .map(({
+          name,
+          address1, city, state, zip,
+          distance
+        }) => [
+          capitalize(name),
+          `${capitalize(address1)} ${capitalize(city)} ${state.toUpperCase()} ${zip}`,
+          distance.toFixed(2)
+        ])
+        data[displayDate] = schools
+      })
+    })
+  })
+} catch(e) { log('Error with CollegeBoard API')}
